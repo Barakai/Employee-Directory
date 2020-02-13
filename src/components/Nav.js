e4988b01376f4eb319033b8c7cbbde6ad5900c1b
Baraka Ibrahim <barackabdow@gmail.com>
2020-02-12 23:17:15 -0600

    updated most of the files

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <h2>Employee Directory</h2>
+    </nav>
+  );
+}
+
+export default Nav;
